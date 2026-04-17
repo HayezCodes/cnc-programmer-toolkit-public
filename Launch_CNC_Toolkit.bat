4f4c5f792d6cae0caeaf0dc3b41a89274edf7b9d
Joshua Hayes <hayesjosh1999@gmail.com>
2026-04-17 11:04:36 -0400

    adopted bat

--- Launch_CNC_Toolkit.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%USERPROFILE%\Desktop\cnc_programming_toolkit\cnc_programming_toolkit"
-python -m streamlit run app.py
-pause
---
+++ run_toolkit.bat
@@ -0,0 +1,7 @@
+@echo off
+cd /d C:\Users\Joshua.Hayes\Desktop\cnc_programming_toolkit
+
+echo Starting CNC Programming Toolkit...
+python -m streamlit run app.py
+
+pause
